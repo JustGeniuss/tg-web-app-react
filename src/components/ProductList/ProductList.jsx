69379dbff454d6add9da0c5669cec56e3c06f2f9
JustGeniuss <danila.kukushkin98@gmail.com>
2022-10-06 14:38:52 +0300

    add form

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ProductList.css';
+
+
+const ProductList = () => {
+    return (
+        <div>
+            ProductList
+        </div>
+
+
+    )
+};
+
+export default ProductList;
