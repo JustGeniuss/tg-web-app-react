3cf84023120296b591afd3ea41b23c613fc2e457
JustGeniuss <danila.kukushkin98@gmail.com>
2022-10-06 15:13:09 +0300

    bag fix

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -22,7 +22,7 @@ const Form = () => {
         return () => {
             tg.offEvent('mainButtonClicked', onSendDate)
         }
-    }, [])
+    }, [onSendDate])
 
     useEffect(() => {
         tg.MainButton.setParams({
